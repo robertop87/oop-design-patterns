0ada99c4355252df62f834fd8a238f23e3f95c58
Luis Roberto Perez <luis.robertop87@gmail.com>
2015-11-04 20:30:23 -0400

    Added basic structure for the projects

---
+++ Main/test/main/MainTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author roberto_perez
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
