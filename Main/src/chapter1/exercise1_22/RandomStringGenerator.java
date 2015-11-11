3e6aee160144dde8c421bcafaf5ce2e21136ad46
vasquezvn <vasquez.vn@gmail.com>
2015-11-11 01:41:06 -0400

    adding solve for exercise 1_22

---
+++ Main/src/chapter1/exercise1_22/RandomStringGenerator.java
@@ -0,0 +1,21 @@
+package chapter1.exercise1_22;
+
+/**
+ *
+ * @author ivan
+ */
+public class RandomStringGenerator {
+    
+    public void addRange(char init, char fin) {
+        
+        
+    }
+    
+    public String nextString(int numbChars) {
+        
+        String generatedString = null;
+        
+        return generatedString;
+        
+    }
+}
